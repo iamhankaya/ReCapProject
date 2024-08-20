ee23836b848a67badd26de6b8c56a7b123a2dc87
Metehan KAYA <210601001@bakircay.edu.tr>
2024-08-20 21:06:01 +0300

    12.gun autofac+validationTool

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -11,19 +14,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ICarService,CarManager>();
-builder.Services.AddSingleton<IBrandService,BrandManager>();
-builder.Services.AddSingleton<IColorService,ColorManager>();
-builder.Services.AddSingleton<ICustomerService,CustomerManager>();
-builder.Services.AddSingleton<IRentalService,RentalManager>();
-builder.Services.AddSingleton<IUserService,UserManager>();
-builder.Services.AddSingleton<ICarDal,EfCarDal>();
-builder.Services.AddSingleton<ICustomerDal,EfCustomerDal>();
-builder.Services.AddSingleton<IColorDal,EfColorDal>();
-builder.Services.AddSingleton<IUserDal,EfUserDal>();
-builder.Services.AddSingleton<IBrandDal,EfBrandDal>();
-builder.Services.AddSingleton<IRentalDal,EfRentalDal>();
 
+builder.Host.UseServiceProviderFactory(services => new AutofacServiceProviderFactory()).ConfigureContainer<ContainerBuilder>(builder => { builder.RegisterModule(new AutofacBusinessModule()); });
 
 var app = builder.Build();
 
